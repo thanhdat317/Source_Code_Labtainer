5dd241debf1ee49dcef004f6f74faf0f6e70a612
Lê Thành Đạt <lethanhdat313@gmail.com>
2026-08-10 21:33:03 +0700

    Fix pregrade.sh exit status and update tarballs

--- web-vulcom/instr_config/pregrade.sh
+++ web-vulcom/instr_config/pregrade.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 homedir=$1
 destdir=$2
-cd $homedir/$destdir
+cd $homedir/$destdir 2>/dev/null || true
 here=`pwd`
-
-is_sqlite=`which sqlite3`
-if [ ! -z $is_sqlite ]; then
-   places=$here/.mozilla/firefox/*default/places.sqlite
-   for fname in $(ls $places 2> /dev/null); do
-     if [[ -f $fname ]]; then
-        outpath=$here/.local/result
-        outfile=$outpath/moz_places.txt
-        mkdir -p "$outpath"
-        sqlite3 "$fname" "SELECT moz_places.* FROM moz_places;" >"$outfile"
-     fi
-   done
-fi
-
 mkdir -p $here/.local/result
+
 if [ -f $here/report_zap.html ] || [ -f $here/report_zap.htm ] || [ -f $here/report_zap ] || [ -f $here/Desktop/report_zap.html ] || [ -f $here/Desktop/traversal.html ]; then
     echo "zap_report_created" > $here/.local/result/zap_report.txt
 fi
@@ -25,3 +12,4 @@ fi
 if [ -f $here/exploit.zip ] || [ -f $here/Desktop/exploit.zip ]; then
     echo "zip_exploit_created" > $here/.local/result/zip_exploit.txt
 fi
+exit 0
